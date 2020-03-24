1b5477ee34db8b91f4ce88e5a28e15d5fdcb1e6c
Jordan Melidor-Fuxis <jordan.melidor@hotmail.fr>
2020-03-24 00:57:48 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -26,6 +26,7 @@ end
 
 def list_of_directors(source)
   # Write this implementation
+  pp source
 end
 
 def total_gross(source)
@@ -38,6 +39,8 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
+  
+  
 end
 
 
