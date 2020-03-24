22872f236540009f102743f0c4f69f6f7098ab1a
Jordan Melidor-Fuxis <jordan.melidor@hotmail.fr>
2020-03-24 01:24:13 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -45,7 +45,7 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
-  total = []
+  total = 0
   count = 0
   while count < source.length do
     
